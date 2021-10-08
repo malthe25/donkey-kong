63bb8dcc38d13da47c679e0e1f23e83c3ed521d0
LasseEngstroem <90896908+LasseEngstroem@users.noreply.github.com>
2021-10-08 11:05:20 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (myMario.isHittingTile(CollisionDirection.Bottom)) {
+        myMario.vy = -100
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (myMario.isHittingTile(CollisionDirection.Bottom)) {
+        myMario.vy = -100
+    }
+})
+let myMario: Sprite = null
 tiles.setTilemap(tilemap`Map1`)
-
-
+myMario = sprites.create(assets.image`Mario`, SpriteKind.Player)
+let myDonkeyKong = sprites.create(img`
+    . . . . . . . f f f f f . . . . 
+    . . . . . . f e e e e e f . . . 
+    . . . . . f e e e d d d d f . . 
+    . . . . f f e e d f d d f d c . 
+    . . . f d d e e d f d d f d c . 
+    . . . c d b e e d d d d e e d c 
+    . . . c d b e e d d d d d d d c 
+    . . . . c f e e e d d c c c c c 
+    . . . . . f f e e d c d d d f . 
+    . . . . f e e e e f f f f f . . 
+    f f . f e e e e e e f f . . . . 
+    f e . f e e f e e f e e f . . . 
+    f e . f e e e f e e f e e f . . 
+    f e f f e f b b f b d f d b f . 
+    f f f f e b d d f d d f d d f . 
+    . f f f f f f f f f f f f f . . 
+    `, SpriteKind.Enemy)
+let myPrincess = sprites.create(img`
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
+    `, SpriteKind.Food)
+controller.moveSprite(myMario)
+myMario.ay = 200
+scene.cameraFollowSprite(myMario)
